a3a76b2d990463c840c661df5e851c6cd8e58c94
matth2k <mrh259@cornell.edu>
2025-06-16 21:16:30 -0400

    add attribute mod

---
+++ src/attribute.rs
@@ -0,0 +1,22 @@
+/*!
+
+  Attributes and parameters for nets and node (gates) in the netlist.
+
+*/
+
+use bitvec::vec::BitVec;
+
+/// A Verilog attribute assigned to a net or gate in the netlist: (* dont_touch *)
+pub type AttributeKey = String;
+/// A Verilog attribute can be assigned a string value: bitvec = (* dont_touch = true *)
+pub type AttributeValue = Option<String>;
+
+/// A dedicated type to parameters to instantiables
+pub enum ParameterType {
+    /// An integer parameter
+    Integer(i32),
+    /// A floating-point parameter
+    Real(f32),
+    /// A bit vector parameter, like for a truth table
+    BitVec(BitVec),
+}
