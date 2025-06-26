96143fafd6671912cf10d1157b11217761a548cc
Matt Hofmann <mrh259@cornell.edu>
2025-06-25 20:58:50 -0400

    Implement attributes (#4)
    * add attributes to circuit nodes
    
    * finish attributes
    
    * fin attributes

---
+++ examples/dont_touch.rs
@@ -0,0 +1,33 @@
+use circuit::{
+    attribute::dont_touch_filter,
+    netlist::{Gate, Netlist},
+};
+
+fn and_gate() -> Gate {
+    Gate::new_logical(
+        "AND".into(),
+        vec!["A".to_string(), "B".to_string()],
+        "Y".to_string(),
+    )
+}
+
+fn main() {
+    let netlist = Netlist::new("example".to_string());
+
+    // Add the the two inputs
+    let a = netlist.insert_input("a".into());
+    let b = netlist.insert_input("b".into());
+
+    // Instantiate an AND gate
+    let instance = netlist
+        .insert_gate(and_gate(), "inst_0".into(), &[a.into(), b.into()])
+        .unwrap();
+
+    // Make this AND gate an output
+    instance.set_attribute("dont_touch".to_string());
+    instance.expose_with_name("y".to_string());
+
+    for nr in dont_touch_filter(&netlist) {
+        println!("Don't touch: {}", nr);
+    }
+}
--- src/attribute.rs
+++ src/attribute.rs
@@ -5,11 +5,58 @@
 */
 
 use bitvec::vec::BitVec;
+use std::collections::{HashMap, HashSet};
+
+use crate::{
+    circuit::{Identifier, Instantiable},
+    netlist::{NetRef, Netlist},
+};
 
 /// A Verilog attribute assigned to a net or gate in the netlist: (* dont_touch *)
 pub type AttributeKey = String;
 /// A Verilog attribute can be assigned a string value: bitvec = (* dont_touch = true *)
 pub type AttributeValue = Option<String>;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+/// An attribute can add information to instances and wires in string form, like 'dont_touch'
+pub struct Attribute {
+    k: AttributeKey,
+    v: AttributeValue,
+}
+
+impl Attribute {
+    /// Create a new attribute pair
+    pub fn new(k: AttributeKey, v: AttributeValue) -> Self {
+        Self { k, v }
+    }
+
+    /// Get the key of the attribute
+    pub fn key(&self) -> &AttributeKey {
+        &self.k
+    }
+
+    /// Get the value of the attribute
+    pub fn value(&self) -> &AttributeValue {
+        &self.v
+    }
+
+    /// Map a attribute key-value pairs to the Attribute struct
+    pub fn from_pairs(
+        iter: impl Iterator<Item = (AttributeKey, AttributeValue)>,
+    ) -> impl Iterator<Item = Self> {
+        iter.map(|(k, v)| Self::new(k, v))
+    }
+}
+
+impl std::fmt::Display for Attribute {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if let Some(value) = &self.v {
+            write!(f, "(* {} = {} *)", self.k, value)
+        } else {
+            write!(f, "(* {} *)", self.k)
+        }
+    }
+}
 
 /// A dedicated type to parameters for instantiables
 pub enum Parameter {
@@ -37,4 +84,77 @@ impl std::fmt::Display for Parameter {
             ),
         }
     }
+}
+
+/// Filter nodes/nets in the netlist by some attribute, like "dont_touch"
+pub struct AttributeFilter<'a, I: Instantiable> {
+    // A reference to the underlying netlist
+    _netlist: &'a Netlist<I>,
+    // The keys to filter by
+    keys: Vec<AttributeKey>,
+    // The set of identifiers that have this attribute
+    set: HashSet<Identifier>,
+    /// The mapping of netrefs that have this attribute
+    map: HashMap<Identifier, NetRef<I>>,
+}
+
+impl<'a, I> AttributeFilter<'a, I>
+where
+    I: Instantiable,
+{
+    /// Create a new filter for the netlist
+    fn new(netlist: &'a Netlist<I>, keys: Vec<AttributeKey>) -> Self {
+        let mut set = HashSet::new();
+        let mut map = HashMap::new();
+        for nr in netlist.objects() {
+            for attr in nr.attributes() {
+                if keys.contains(attr.key()) {
+                    if let Some(inst) = nr.get_instance_name() {
+                        set.insert(inst.clone());
+                        map.insert(inst.clone(), nr.clone());
+                    }
+                    for net in nr.nets() {
+                        set.insert(net.get_identifier().clone());
+                    }
+                }
+            }
+        }
+        Self {
+            _netlist: netlist,
+            keys,
+            set,
+            map,
+        }
+    }
+
+    /// Check if an identifier has one of the attributes
+    pub fn has(&self, id: &Identifier) -> bool {
+        self.set.contains(id)
+    }
+
+    /// Return a slice to the keys that were used for filtering
+    pub fn keys(&self) -> &[AttributeKey] {
+        &self.keys
+    }
+}
+
+impl<'a, I> IntoIterator for AttributeFilter<'a, I>
+where
+    I: Instantiable,
+{
+    type Item = NetRef<I>;
+
+    type IntoIter = std::collections::hash_map::IntoValues<Identifier, NetRef<I>>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.map.into_values()
+    }
+}
+
+/// Returns a filtering of nodes and nets that are marked as 'dont_touch'
+pub fn dont_touch_filter<'a, I>(netlist: &'a Netlist<I>) -> AttributeFilter<'a, I>
+where
+    I: Instantiable,
+{
+    AttributeFilter::new(netlist, vec!["dont_touch".to_string()])
 }
