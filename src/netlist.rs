ba65b6ad22959df8a4740a49819dd7f02f41ce53
matth2k <mrh259@cornell.edu>
2026-01-27 01:51:56 -0500

    add is empty

--- src/netlist.rs
+++ src/netlist.rs
@@ -1613,6 +1613,11 @@ where
         self.objects.borrow().len()
     }
 
+    /// Returns `true` if the netlist contains no objects.
+    pub fn is_empty(&self) -> bool {
+        self.objects.borrow().is_empty()
+    }
+
     /// Returns `true` if an output of `netref` which is driving a module output.
     pub fn drives_an_output(&self, netref: NetRef<I>) -> bool {
         let my_index = netref.unwrap().borrow().get_index();
